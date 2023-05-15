abc2c280102710d053303b242457f413dd20731d
shoebbint <shoebbint@gmail.com>
2023-05-15 12:55:21 +0600

    error-handling

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors =require('cors');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
-const port = process.env.PORT||5000;
+const port=process.env.PORT||5000;
 //middleware
 app.use(cors());
 app.use(express.json())
@@ -85,5 +85,5 @@ app.get('/', (req, res) => {
   })
   
   app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port, ${port}`)
   })
